8ea2232b5eaafaa8ea61bf3ef53a263cfd7973a1
Matt Welter <welter.m@icloud.com>
2021-03-08 15:37:34 -0700

    Initial commit

--- app.py
+++ app.py
@@ -1,89 +1,77 @@
+import imghdr
 import os
-from flask import Flask, request, redirect, url_for, render_template, send_from_directory,flash 
-from werkzeug.utils import secure_filename
-import cv2
 import numpy as np
+import cv2
+from PIL import Image
+from os.path import join, dirname, realpath
+from flask import Flask, render_template, request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
+import json
 
-UPLOAD_FOLDER ='static/uploads/'
-DOWNLOAD_FOLDER = 'static/downloads/'
-ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
-app = Flask(__name__, static_url_path="/static")
+UPLOADS_PATH = join(dirname(realpath(__file__)), 'static/uploads/..')
 
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
 
-# APP CONFIGURATIONS
-app.config['SECRET_KEY'] = 'opencv'  
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
-# limit upload size upto 6mb
-app.config['MAX_CONTENT_LENGTH'] = 6 * 1024 * 1024
+def validate_image(stream):
+    header = stream.read(512)
+    stream.seek(0) 
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file attached in request')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            flash('No file selected')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            process_file(os.path.join(UPLOAD_FOLDER, filename), filename)
-            data={
-                "processed_img":'static/downloads/'+filename,
-                "uploaded_img":'static/uploads/'+filename
-            }
-            return render_template("index.html",data=data)  
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+                file_ext != validate_image(uploaded_file.stream):
+            abort(400)
+        uploaded_file.save('new.png')
+   
+    #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+    face_cascade = cv2.CascadeClassifier('/Users/matt/Python/LazerEyes/haarcascade_eye.xml')
 
-def process_file(path, filename):
-    detect_object(path, filename)
+    #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+    eye_cascade = cv2.CascadeClassifier('/Users/matt/Python/LazerEyes/haarcascade_eye.xml')
+
     
+    img = cv2.imread('new.png')
+    dot = cv2.imread('dot_transparent.png', cv2.IMREAD_UNCHANGED)
 
-def detect_object(path, filename):    
-    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-        "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-        "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-        "sofa", "train", "tvmonitor"]
-    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
-    prototxt="ssd/MobileNetSSD_deploy.prototxt.txt"
-    model ="ssd/MobileNetSSD_deploy.caffemodel"
-    net = cv2.dnn.readNetFromCaffe(prototxt, model)
-    image = cv2.imread(path)
-    image = cv2.resize(image,(480,360))
-    (h, w) = image.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
-    net.setInput(blob)
-    detections = net.forward()
-    for i in range(0, detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > 0.60:
-            idx = int(detections[0, 0, i, 1])
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-            # display the prediction
-            label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
-            # print("[INFO] {}".format(label))
-            cv2.rectangle(image, (startX, startY), (endX, endY),
-                COLORS[idx], 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(image, label, (startX, y),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    cv2.imwrite(f"{DOWNLOAD_FOLDER}{filename}",image)
-  
-# download 
-# @app.route('/uploads/<filename>')
-# def uploaded_file(filename):
-#     return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
+    for (x, y, w, h) in faces:
 
-if __name__ == '__main__':
-    app.run()
+        eyes = eye_cascade.detectMultiScale(gray[y:y+h, x:x+w])
+
+        for (ex, ey, ew, eh) in eyes:
+
+            # Filter out small detections, if you're only want to have the eyes
+            if ew < 100 or eh < 100:
+                continue
+
+            d = cv2.resize(dot.copy(), (eh, ew))
+            d_alpha = d[..., 3] / 255.0
+            d_alpha = np.repeat(d_alpha[..., np.newaxis], 3, axis=2)
+            d = d[..., :3]
+
+            img[y+ey:y+ey+eh, x+ex:x+ex+ew, :] = \
+                img[y+ey:y+ey+eh, x+ex:x+ex+ew, :] * (1 - d_alpha) + d * d_alpha
+
+    cv2.imwrite('out.png', img)
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
